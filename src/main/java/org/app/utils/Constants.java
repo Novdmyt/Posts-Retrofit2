11561c62f4da5d18096ad6c350537b4205134898
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-14 14:19:41 +0300

    Create class Constants

---
+++ src/main/java/org/app/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.app.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    public final static String NO_DATA_MSG = "\n>> No data or internet is out.";
+    public final static String WRONG_ID_MSG = "\n>> ID should be an integer between 1 and 100";
+
+}
