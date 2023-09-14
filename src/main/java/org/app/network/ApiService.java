200592a6a8f27561fe0bb885de88feefe379c7a9
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-14 14:23:47 +0300

    Create method getPosts

---
+++ src/main/java/org/app/network/ApiService.java
@@ -0,0 +1,16 @@
+package org.app.network;
+
+import org.app.entity.Post;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import java.util.List;
+
+public interface ApiService {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+
+}
+
