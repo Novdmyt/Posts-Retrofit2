e1da1077ed2ce5c940d347aa9fe89bcb6d702fb1
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-14 14:36:42 +0300

    Create constructor PostController

---
+++ src/main/java/org/app/controller/PostController.java
@@ -0,0 +1,14 @@
+package org.app.controller;
+
+import org.app.model.PostModel;
+import org.app.view.PostView;
+
+public class PostController {
+    private final PostModel model;
+    private final PostView view;
+
+    public PostController(PostModel model, PostView view) {
+        this.model = model;
+        this.view = view;
+    }
+}
