a062f37aa833add49ad9f8f1a21dfc9d308f4207
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-14 15:15:51 +0300

    Add method  filterChoice

--- src/main/java/org/app/controller/AppController.java
+++ src/main/java/org/app/controller/AppController.java
@@ -4,13 +4,19 @@ import org.app.model.AppModel;
 import org.app.view.AppView;
 
 public class AppController {
-    public class AppController {
+    AppModel model;
+    AppView view;
 
-        AppModel model;
-        AppView view;
-
-        public AppController(AppModel model, AppView view) {
-            this.model = model;
-            this.view = view;
+    public AppController(AppModel model, AppView view) {
+        this.model = model;
+        this.view = view;
+    }
+     private void filterChoice(String choice) {
+        switch (choice) {
+            case "1" -> model.readPost();
+            case "2" -> model.readPostById();
+            case "0" -> view.getOutput(" App closed");
+            default -> view.getOutput("Unexpected value: " + choice);
         }
+    }
 }
