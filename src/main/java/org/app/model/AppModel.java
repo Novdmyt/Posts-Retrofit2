86052871a0876356b29cb04666120a8c8efb847e
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-14 15:21:50 +0300

    Add method readPostById

--- src/main/java/org/app/model/AppModel.java
+++ src/main/java/org/app/model/AppModel.java
@@ -1,5 +1,6 @@
 package org.app.model;
 
+import org.app.controller.PostController;
 import org.app.view.PostView;
 
 public class AppModel {
@@ -9,5 +10,11 @@ public class AppModel {
         PostView view = new PostView();
         PostController controller = new PostController(model, view);
         controller.getPosts();
+    }
+    public void readPostById() {
+        PostModel model = new PostModel();
+        PostView view = new PostView();
+        PostController controller = new PostController(model, view);
+        controller.getPostById();
     }
 }
