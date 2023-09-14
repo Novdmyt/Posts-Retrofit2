eb92979d721519679f87bb499df61f442ab9e437
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-14 15:08:37 +0300

    Add method  readPostById

--- src/main/java/org/app/controller/PostController.java
+++ src/main/java/org/app/controller/PostController.java
@@ -1,8 +1,14 @@
 package org.app.controller;
 
+import org.app.entity.Post;
 import org.app.model.PostModel;
 import org.app.utils.AppStarter;
+import org.app.utils.Constants;
 import org.app.view.PostView;
+import retrofit2.Response;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class PostController {
     private final PostModel model;
@@ -12,5 +18,16 @@ public class PostController {
         this.model = model;
         this.view = view;
     }
+    private String readPostById(int id) {
+        Optional<Response<Post>> optional = model.fetchPostById(id);
 
+        if (optional.isPresent()) {
+            Post post = optional.get().body();
+
+            if (Objects.nonNull(post)) {
+                return post.toString();
+            }
+        }
+        return Constants.NO_DATA_MSG;
+    }
 }
